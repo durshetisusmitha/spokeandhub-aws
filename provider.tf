71d830618b9761c482832945337bdb425ed194db
durshetisusmitha <82156615+durshetisusmitha@users.noreply.github.com>
2024-01-11 11:24:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXYKJQRR2KGQES4HO"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
